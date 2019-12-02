21dde34a77f45b85b6ce22ee31085cecdedc99bc
SLOkamTech <upendra.kris@gmail.com>
2019-12-02 14:48:03 +0530

    Hello

--- src/main/java/com/slokam/hr/LoginService.java
+++ src/main/java/com/slokam/hr/LoginService.java
@@ -3,7 +3,7 @@ package com.slokam.hr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
+// changes by user2
 @Service
 public class LoginService implements ILoginService {
 	@Autowired
