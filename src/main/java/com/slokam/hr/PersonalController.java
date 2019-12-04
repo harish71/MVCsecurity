73a72e2a4296026238cf568791e515e838d03927
pradeepkamal <gollamudipradeep@gmail.com>
2019-12-04 18:09:52 +0530

    som chngs in persncon and prsnjsp

--- src/main/java/com/slokam/hr/PersonalController.java
+++ src/main/java/com/slokam/hr/PersonalController.java
@@ -1,17 +1,33 @@
 package com.slokam.hr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.slokam.hr.entity.Personal;
+import com.slokam.hr.service.impl.PersonalServiceImpl;
+
+
 @Controller
 @RequestMapping("hr")
 public class PersonalController {
-	
+	@Autowired
+	private PersonalServiceImpl psimpl;
 	@RequestMapping("/personalPage")
 	public ModelAndView personalPage()
 	{
 		ModelAndView mv=new ModelAndView("personal");
+		return mv;
+	}
+	
+	@RequestMapping("/save")
+	public ModelAndView savePage(Personal person)
+	{
+		psimpl.save(person);
+		
+		ModelAndView mv=new ModelAndView("personal");
+		
 		return mv;
 	}
 
--- src/main/webapp/jsp/personal.jsp
+++ src/main/webapp/jsp/personal.jsp
@@ -8,7 +8,13 @@
 <form action="save">
 Lastname:<input type="text" name="lastName"><br>
 Firstname:<input type="text" name="firstName"><br>
-middlename:<input type="text" name="middleName">
+middlename:<input type="text" name="middleName"><br>
+Gender:<input type="text" name="gender"><br>
+MaritalStatus:<input type="text" name="maritalStatus"><br>
+State:<input type="text" name="state"><br>
+Religion:<input type="text" name="religion"><br>
+Nationality:<input type="text" name="nationality"><br>
+<input type="submit" value="save">
 </form>
 
 </head>
